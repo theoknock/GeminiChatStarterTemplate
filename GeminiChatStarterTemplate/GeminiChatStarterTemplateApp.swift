fb83ca4a41f1831223716981ff27b8a687552ace
James Alan Bush <jamesalanbush1972@gmail.com>
2024-06-04 23:59:23 -0400

    Initial Commit

---
+++ GeminiChatStarterTemplate/GeminiChatStarterTemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeminiChatStarterTemplateApp.swift
+//  GeminiChatStarterTemplate
+//
+//  Created by Xcode Developer on 6/4/24.
+//
+
+import SwiftUI
+
+@main
+struct GeminiChatStarterTemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
