a9608c92bc417a824e371d037f084df98d3e6481
James Alan Bush <jamesalanbush1972@gmail.com>
2024-06-05 00:12:07 -0400

    !!!

--- GeminiChatStarterTemplate/ContentView.swift
+++ GeminiChatStarterTemplate/ContentView.swift
@@ -7,14 +7,24 @@ import GoogleGenerativeAI
 struct ContentView: View {
     @State private var gemini: GeminiChatStarterTemplate = GeminiChatStarterTemplate()
     
+    let toneBarrierSapphire: Color = Color.init(hue: 206 / 360, saturation: 1, brightness: 1)
+    
+    
     var body: some View {
         Spacer()
         VStack {
             Spacer()
-            Text("♊︎")
-                .font(.largeTitle)
-            Spacer()
-            Text($gemini.responseText.wrappedValue)
+            ZStack {
+                Text("♊︎")
+                    .font(.system(size: 244))
+                    .scaledToFit()
+                    .aspectRatio(1.0, contentMode: .fit)
+                    .frame(width: UIScreen.main.bounds.size.width)
+                    .clipShape(Rectangle())
+                    .foregroundStyle(toneBarrierSapphire.opacity(0.15))
+                Spacer()
+                Text($gemini.responseText.wrappedValue)
+            }
             TextField("Send a message to Gemini...", text: $gemini.promptText)
                 .padding()
                 .border(.blue, width: 1.0)
